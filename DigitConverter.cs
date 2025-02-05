5d57c0609f206881e4f90b50208fbf7aaec8d670
Joshua Peck <peckjj@dukes.jmu.edu>
2025-02-04 22:51:42 -0500

    Generating Hashes... slowly...

---
+++ DigitConverter.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace HashMapper
+{
+    internal class DigitConverter
+    {
+        public static string convertFromBase10(BigInteger value, char[] charset)
+        {
+            int nBase = charset.Length;
+
+            if (nBase == 0)
+            {
+                throw new ArgumentException("Must have at least one character in charset.");
+            }
+            if (nBase == 1)
+            {
+                // Thought you could get me with this one?
+                StringBuilder sb = new StringBuilder();
+                while (value != 0)
+                {
+                    sb.Append(charset[0]);
+                    value--;
+                }
+
+                return sb.ToString();
+            }
+            if (value == BigInteger.Zero)
+            {
+                return "" + charset[0];
+            }
+
+            string result = "";
+
+            while (value > 0)
+            {
+                result = charset[((int)(value % nBase))] + result;
+                value = value / nBase;
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using ArgParseLib;
 using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Numerics;
+using System.Text;
 
 namespace HashMapper
 {
     internal class Program
     {
         static readonly string DEFAULT_CHAR_SET_TEXT = "abcdefghijklmnopqrstuvwxyz1234567890`~-_=+[]{}\\/|;:'\",.<>?!@#$%^&*()";
+        static readonly string DEFAULT_TABLE_NAME = "hasher";
 
         static int Main(string[] args)
         {
@@ -13,6 +17,9 @@ namespace HashMapper
             string dbFilePath = "";
             bool displayHelp = false;
             bool resetDb = false;
+            string tableName = DEFAULT_TABLE_NAME;
+            BigInteger minIdx = BigInteger.Zero;
+            BigInteger maxIdx = 100;
 
             OptionParam[] options =
             {
@@ -31,6 +38,18 @@ namespace HashMapper
                 new Flag("-r|--reset", "Reset the database of stored hashes.", () =>
                 {
                     resetDb = true;
+                }),
+                new Option("-t|--table-name", "tableName", "Custom table name for storing hashes for a particular charset", (value) =>
+                {
+                    tableName = value;
+                }),
+                new Option("-m|--min", "minValue", "Lower bound index to start generating hashes", (value) =>
+                {
+                    minIdx = BigInteger.Parse(value);
+                }),
+                new Option("-M|--max", "maxValue", "Upper bound index to start generating hashes", (value) =>
+                {
+                    maxIdx = BigInteger.Parse(value);
                 })
             };
 
@@ -41,6 +60,17 @@ namespace HashMapper
             {
                 parser.Usage();
                 return 0;
+            }
+
+            if (minIdx < 0)
+            {
+                Console.Error.WriteLine("Min Index is less than 0, this is not allowed.");
+                return -1;
+            }
+            if(minIdx >= maxIdx)
+            {
+                Console.Error.WriteLine("Min Index is greater than or equal to Max Index, this is not allowed.");
+                return -1;
             }
 
             string rawCharSetText;
@@ -95,67 +125,146 @@ namespace HashMapper
                 Mode = SqliteOpenMode.ReadWriteCreate
             }.ToString();
 
-            using (var connection = new SqliteConnection(conString))
+            SqliteConnection con = new SqliteConnection(conString);
+
+            if (string.IsNullOrEmpty(rawCharSetText))
             {
-                if (resetDb)
-                {
-                    setupDb(connection);
-                }
-
-                connection.Open();
-
-                if (string.IsNullOrEmpty(rawCharSetText))
-                {
-                    rawCharSetText = DEFAULT_CHAR_SET_TEXT;
-                }
-
-                HashSet<char> charset = new HashSet<char>(rawCharSetText.Trim().ToCharArray());
-
-                Console.WriteLine($"Char set loaded with {charset.Count} characters:\n");
-                foreach (char c in charset)
-                {
-                    Console.Write(c);
-                }
-
-                Console.WriteLine();
+                rawCharSetText = DEFAULT_CHAR_SET_TEXT;
             }
 
+            char[] charset = new HashSet<char>(rawCharSetText.Trim().ToCharArray()).ToArray();
+
+            Array.Sort(charset);
+
+            Console.WriteLine($"Char set loaded with {charset.Length} characters:\n");
+            Console.Write(string.Concat(charset));
+
+            Console.WriteLine();
+
+            con.Open();
+
+            if (resetDb)
+            {
+                setupDb(con, charset.ToArray(), tableName);
+            }
+
+            Console.WriteLine("First character sequence: " + DigitConverter.convertFromBase10(minIdx, charset));
+
+            while (minIdx < maxIdx)
+            {
+                if (minIdx % 500 == 0)
+                {
+                    Console.WriteLine(minIdx);
+                }
+                string input = DigitConverter.convertFromBase10(minIdx, charset);
+                insertHash(con, ((int)minIdx), input, generateHash(input), tableName);
+                minIdx++;
+            }
+
+            con.Close();
             return 0;
         }
 
-        private static void setupDb(SqliteConnection connection)
+        private static string generateHash(string input)
         {
-            using (connection)
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
             {
-                connection.Open();
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
 
-                Console.WriteLine("Initializing database...");
-                var command = connection.CreateCommand();
-                command.CommandText =
-                @"
-                    DROP TABLE IF EXISTS hashes;
-                    CREATE TABLE ""hashes"" (
-	                    ""text""	TEXT NOT NULL UNIQUE,
-	                    ""md5_hash""	TEXT NOT NULL,
-	                    PRIMARY KEY(""text"")
-                    );
-                ";
+                return Convert.ToHexString(hashBytes); // .NET 5 +
 
-                Console.WriteLine(command.CommandText);
+                // Convert the byte array to hexadecimal string prior to .NET 5
+                // StringBuilder sb = new System.Text.StringBuilder();
+                // for (int i = 0; i < hashBytes.Length; i++)
+                // {
+                //     sb.Append(hashBytes[i].ToString("X2"));
+                // }
+                // return sb.ToString();
+            }
+        }
 
+        private static void dropAllTables(SqliteConnection con)
+        {
+            var command = con.CreateCommand();
+
+            List<string> tables = new();
+
+            command.CommandText =
+            @"
+                SELECT tbl_name FROM sqlite_master WHERE type=""table"";
+            ";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                tables.Add(reader.GetString(0));
+            }
+            foreach (var table in tables)
+            { 
+                command = con.CreateCommand();
+                command.CommandText = $"DROP TABLE {table};";
                 command.ExecuteNonQuery();
             }
         }
 
-        private static void insertHash(SqliteConnection connection, string input, string hash)
+        private static void createHashTable(SqliteConnection con, string tableName, string charset, string last_index)
         {
-            if (connection.State != System.Data.ConnectionState.Open)
-            {
-                throw new Exception("Database connection is not opened.");
-            }
+            Console.WriteLine("Creating table " +  tableName);
 
-            var command = connection.CreateCommand();
-            command.CommandText = "INSERT INTO hashes VALUES ($input, $hash);";
+            var command = con.CreateCommand();
+            command.CommandText =
+            $@"
+            DROP TABLE IF EXISTS {tableName};
+            CREATE TABLE {tableName} (
+	           ""id""	INTEGER NOT NULL UNIQUE,
+	            ""text""	TEXT NOT NULL UNIQUE,
+	            ""md5_hash""	TEXT NOT NULL,
+	            PRIMARY KEY(""id"")
+            );
+            ";
+            command.ExecuteNonQuery();
+
+            command = con.CreateCommand();
+            command.CommandText =
+            @"
+            INSERT INTO hash_tables VALUES ($t, $cs, $li);
+            ";
+            command.Parameters.AddWithValue("$t", tableName);
+            command.Parameters.AddWithValue("$cs", charset);
+            command.Parameters.AddWithValue("$li", last_index);
+            command.ExecuteNonQuery();
+        }
+
+        private static void createMetaTable(SqliteConnection con)
+        {
+            var command = con.CreateCommand();
+            command.CommandText =
+            @"
+            DROP TABLE IF EXISTS hash_tables;
+            CREATE TABLE ""hash_tables"" (
+                    ""hash_table_name""	TEXT NOT NULL UNIQUE,
+                    ""charset""	TEXT NOT NULL UNIQUE,
+                    ""last_index""	TEXT NOT NULL,
+                    PRIMARY KEY(""hash_table_name"")
+            );
+            ";
+            command.ExecuteNonQuery();
+        }
+
+        private static void setupDb(SqliteConnection con, char[] charset, string tableName)
+        {
+            Console.WriteLine("Initializing database...");
+
+            dropAllTables(con);
+            createMetaTable(con);
+            createHashTable(con, tableName, string.Concat(charset), "0");
+        }
+
+        private static void insertHash(SqliteConnection con, int id, string input, string hash, string tableName)
+        {
+            var command = con.CreateCommand();
+            command.CommandText = $"INSERT INTO {tableName} VALUES ($id, $input, $hash);";
+            command.Parameters.AddWithValue("$id", id);
             command.Parameters.AddWithValue("$input", input);
             command.Parameters.AddWithValue("$hash", hash);
             command.ExecuteNonQuery();
